c54bc20567a6e84cdc03f02d5e5dece0c21ec2b5
Björn Hellander <bjorn.hellander@gmail.com>
2022-07-28 11:12:28 +0200

    Added MSTest projects

---
+++ MSTest2/UnitTestC.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest2
+{
+    [TestClass]
+    public class UnitTestC : MSTest1.UnitTestC
+    {
+    }
+}
---
+++ MSTest2/UnitTestD.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest2
+{
+    [TestClass]
+    public class UnitTestD
+    {
+        [TestMethod]
+        public void TestD1()
+        {
+        }
+    }
+}
